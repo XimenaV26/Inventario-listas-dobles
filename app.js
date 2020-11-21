bb5e845f797511c3bc22fb7e2bc65fae38211bf4
XimenaV26 <54459424+XimenaV26@users.noreply.github.com>
2020-11-20 22:40:03 -0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,31 @@
+const miInfo= new Array();
+
+const btnAgregar=document.getElementById("btnAgregar");
+btnAgregar.addEventListener("click",e =>{
+    /*
+    Recuperar cada caja de texto
+    Crear un producto
+    Mostrar en Div details y su informacion
+    Agregarlo al Array
+      
+    */
+
+    let nombre= document.getElementById("textNombre").value; 
+    let descripcion = document.getElementById("textDescripcion").value;
+    let cantidad= document.getElementById("textCantidad").value;
+    let costo = document.getElementById("textCosto").value;
+
+    let product= new Producto(nombre, descripcion, cantidad, costo);
+
+    let details= document.getElementById("details");
+    details.innerHTML += `
+    <div class= "p-3 mb-2 bg-info text-white p-3 m-3" > 
+    <p><strong>ID ${product.id} - </strong>${product._nombre}</p>
+    <p>Descripcion: ${product._descripcion}</p>
+    <p>Valor total: ${product.valorMercancia}</p>
+    <p>${product.mostrarInfo()}</p>
+    </div>
+
+    `;
+    console.log(product.mostrarInfoJSon);
+});
