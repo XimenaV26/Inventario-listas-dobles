96cad1d03eb793f53101e1e6613aa9c4a8948170
XimenaV26 <54459424+XimenaV26@users.noreply.github.com>
2020-11-21 20:13:14 -0600

    Update estructura.js

--- estructura.js
+++ estructura.js
@@ -1,12 +1,12 @@
 class Estructura{
     constructor(){
         this._incio= null; 
-        this.totalNodes=0; 
+        this._final= null; 
     }
 
     agregar(producto){
-        if(this.incio===null){
-            this.incio=producto;
+        if(this._incio===null){
+            this._incio=producto;
         }
         else{
             let aux, aux2
@@ -31,11 +31,79 @@ class Estructura{
     }
 
     agregarInicio(producto){
-         producto.siguente=this.incio;
-         this.incio=producto;
+         producto._siguente=this._incio;
+         this._incio=producto;
     }
 
     buscar(codigo){
-        
+        if(this._incio===null){
+            return false
+        }
+        else if(this._incio.codigo() == codigo){
+            return this._incio;
+        }
+        else{
+            let aux= this._incio; 
+            while(aux.mostrarSiguiente()!= null && aux.mostrarSiguiente().codigo() != codigo){
+                aux=aux.mostrarSiguiente();
+            }
+            if(aux.mostrarSiguiente()===null){
+                return false
+            }else{
+                return aux.mostrarSiguiente();
+            }
+        }  
+    }
+
+    borrar(codigo){
+        let producto= this.buscar(codigo), aux;
+        if(producto===false){
+            return false
+        }else if(this._incio.codigo() === codigo){
+            if(this._incio.mostrarSiguiente()!=null){
+                this._incio= this._incio.mostrarSiguiente()
+                this._incio.CAnterior(null)
+            }else{
+                this._incio=null; 
+            }
+        }else if(producto.mostrarSiguiente()!=null){
+            aux=producto.mostrarAnterior();
+            aux.CSiguiente(producto.mostrarSiguiente())
+            aux.mostrarSiguiente().CAnterior(aux)
+
+            producto.CAnterior(null)
+            producto.CSiguiente(null)
+
+        }else{
+            aux=producto.mostrarAnterior()
+            aux.CSiguiente(producto.mostrarSiguiente())
+
+            producto.CAnterior(null);
+            producto.CSiguiente(null);
+        }
+
+        return producto
+    }
+
+    listar(){
+        let salida= "";
+       aux=this._incio;
+       while(aux!=null){
+           salida+= "<p>" + aux.mostrarInfo() + "</p>"
+           aux=aux._siguente;
+       }
+       return salida;
+    }
+
+    borrarPrimero(){
+        if(this._incio==null){
+            return false
+        }else{
+            this._incio=this._incio.mostrarSiguiente()
+            if(this._incio != null){
+                this._incio.CAnterior(null)
+            }
+            return this._incio
+        }
     }
 }
